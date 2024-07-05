51d1aa493892def9b20f9460c059518851c8008f
jbry <jer.bryant@gmail.com>
2024-07-05 15:53:20 -0500

    Update to build new ESP32S3

--- src/platforms/esp/32/clockless_rmt_esp32.cpp
+++ src/platforms/esp/32/clockless_rmt_esp32.cpp
@@ -336,15 +336,15 @@ void IRAM_ATTR ESP32RMTController::tx_start()
     RMT.chnconf0[mRMT_channel].tx_start_n = 1;
 #elif CONFIG_IDF_TARGET_ESP32S2 || CONFIG_IDF_TARGET_ESP32
     // rmt_ll_tx_reset_pointer(&RMT, mRMT_channel)
-    RMT.conf_ch[mRMT_channel].conf1.mem_rd_rst = 1;
-    RMT.conf_ch[mRMT_channel].conf1.mem_rd_rst = 0;
+    RMT.conf_ch[mRMT_channel].conf1.mem_rd_rst_chn = 1;
+    RMT.conf_ch[mRMT_channel].conf1.mem_rd_rst_chn = 0;
     // rmt_ll_clear_tx_end_interrupt(&RMT, mRMT_channel)
     RMT.int_clr.val = (1 << (mRMT_channel * 3));
     // rmt_ll_enable_tx_end_interrupt(&RMT, mRMT_channel, true)
     RMT.int_ena.val &= ~(1 << (mRMT_channel * 3));
     RMT.int_ena.val |= (1 << (mRMT_channel * 3));
     // rmt_ll_tx_start(&RMT, mRMT_channel)
-    RMT.conf_ch[mRMT_channel].conf1.tx_start = 1;
+    RMT.conf_ch[mRMT_channel].conf1.tx_start_chn = 1;
 #else
     #error Not yet implemented for unknown ESP32 target
 #endif
@@ -397,10 +397,10 @@ void IRAM_ATTR ESP32RMTController::doneOnChannel(rmt_channel_t channel, void * a
     // rmt_ll_enable_tx_end_interrupt(&RMT, channel)
     RMT.int_ena.val &= ~(1 << (channel * 3));
     // rmt_ll_tx_stop(&RMT, channel)
-    RMT.conf_ch[channel].conf1.tx_stop = 1;
+    RMT.conf_ch[channel].conf1.tx_stop_chn = 1;
     // rmt_ll_tx_reset_pointer(&RMT, channel)
-    RMT.conf_ch[channel].conf1.mem_rd_rst = 1;
-    RMT.conf_ch[channel].conf1.mem_rd_rst = 0;
+    RMT.conf_ch[channel].conf1.mem_rd_rst_chn = 1;
+    RMT.conf_ch[channel].conf1.mem_rd_rst_chn = 0;
 #elif CONFIG_IDF_TARGET_ESP32
     // rmt_ll_enable_tx_end_interrupt(&RMT, channel)
     RMT.int_ena.val &= ~(1 << (channel * 3));
